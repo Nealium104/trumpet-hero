7b2aa99989d992f598738b572a0e34a54b531a4b
Nealium104 <neal.powers@outlook.com>
2025-11-18 22:38:26 -0500

    add a basic readerfor note input to fingering output

---
+++ cell-reader.js
@@ -0,0 +1,13 @@
+import noteTable from "./notes.json" with { type: "json" };
+
+const cell = ['C4', 'D4', 'E4', 'G4'];
+
+function getCellFingering(notes) {
+    const collect = [];
+    for (const note of notes) {
+        collect.push(noteTable[note]);
+    }
+    return collect
+}
+
+console.log(getCellFingering(cell));
